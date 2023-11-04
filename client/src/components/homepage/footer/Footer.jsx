62841dcb37c18e81f8fd8195592eceaf33955575
Ayush Bansal <116891199+Bansalayush247@users.noreply.github.com>
2023-11-04 11:48:42 +0530

    Add files via upload

---
+++ client/src/components/homepage/footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+export default function Footer() {
+    return (
+        <footer className="footer-container">
+            <a className="footer-link" href="https://www.linkedin.com/in/bansalayush247/">LinkedIn</a>
+            <a className="footer-link" href="https://twitter.com/yashg_twt">Twitter</a>
+            <a className="footer-link">Instagram</a>
+            <p className="copyright">© CipherCrackHeads.</p>
+        </footer>
+    );
+}
